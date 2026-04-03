d741ebc4824c74652b1068509232e1f297019711
leonardy24 <leonardyescalona@gmail.com>
2026-04-03 20:46:04 +0200

    modificacion setting.py

--- rifasPueblosDelSur/rifasPueblosDelSur/settings.py
+++ rifasPueblosDelSur/rifasPueblosDelSur/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'rifasPueblosDelSur/static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 LOGIN_REDIRECT_URL = '/'  # Redirige al inicio después de iniciar sesión
 LOGOUT_REDIRECT_URL = '/login/'  # Redirige al inicio después de cerrar sesión
